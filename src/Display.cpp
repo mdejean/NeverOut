b62447de8abda0af0f36183b2b86b7d7ba364068
mdejean <marcel.dejean@alumni.case.edu>
2016-02-21 04:03:05 -0500

    use stringstream for display

--- src/Display.cpp
+++ src/Display.cpp
@@ -6,16 +6,29 @@
  */
 
 #include "Display.h"
+#include <string>
+#include <sstream>
 
 Display::Display() : display(0) {
-	// TODO Auto-generated constructor stub
-
+	display.clear();
+	display.setColor(155, 155, 120);
 }
 
 Display::~Display() {
-	// TODO Auto-generated destructor stub
+	display.clear();
 }
 
 void Display::write(ScaleValue f, ScaleValue e, ScaleValue cur, ScaleRate r, bool p, bool w, float warn_p) {
 	//TODO
+	std::stringstream ss;
+	ss << cur.stringWithUnits();
+	ss << " (" << f.string() << "-" << e.string() << ") @" << r << "\n";
+	if (p) {
+		ss << "picked up ";
+	}
+	if (w) {
+		ss << "low ";
+	}
+	ss << ScaleValue::interpolate(e, f, warn_p).string();
+	display.write(ss.str());
 }
